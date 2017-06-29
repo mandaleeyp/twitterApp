accb0cb2c931cd5e61e978dee2a450829d9a67c3
Amanda Lee <mandaleeyp@MacBook-Pro-8.dhcp.thefacebook.com>
2017-06-29 14:59:30 -0700

    added reply button (not functional yet)

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -23,8 +23,8 @@ import com.loopj.android.http.RequestParams;
 public class TwitterClient extends OAuthBaseClient {
 	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance(); // Change this
 	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = "1XEM9aQ2YKBeSNQlbKCAYdWlk";       // Change this
-	public static final String REST_CONSUMER_SECRET = "St5VvqAqJb6WZCsHQtoe4yDrp1a6dO0kQxAfgrasPSR699QnYO"; // Change this
+	public static final String REST_CONSUMER_KEY = "2r2fdtyXUf18noaXlAVjpvs82";       // Change this
+	public static final String REST_CONSUMER_SECRET = "FzQbW5KRVsM2modPR2eQ2tgpKOQH4gfAnUZ5wHzmIcg8qNNpXf"; // Change this
 
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
 	public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
